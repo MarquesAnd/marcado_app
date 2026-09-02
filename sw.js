3cf24a41aae45b76f56fa517b297d32572d5868a
André Marques <111473714+MarquesAnd@users.noreply.github.com>
2026-09-02 11:26:33 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'marcado-v3';
+const CACHE = 'marcado-v4';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-180.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
